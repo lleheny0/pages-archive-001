0e31a80a2cdff646b4d4f181f6f053e40f5d8352
Luke Leheny <lleheny@umass.edu>
2015-12-08 00:06:09 -0500

    test

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-function chooseTextColor(hex) {
-}
-
 $(document).ready(function() {
-    $('#color-input').on('input', function() {
-        $('#color-input-card').css('background-color', '#' + $('#color-input').val())
-    });
+    $('#page').load('./title.html');
 });
