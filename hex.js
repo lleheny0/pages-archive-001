f7ee7e71db430cccdd600709d9ef3ba286646dc0
Luke Leheny <lleheny@umass.edu>
2015-12-11 23:38:55 -0500

    style, debugging too

--- hex.js
+++ hex.js
@@ -1,9 +1,8 @@
+//# sourceURL=hex.js
+
 function setupEvents() {
     $('#hex-input').on('input', function() {
         var color = $('#hex-input').val();
-        if (color.length === 7) {
-            color = color.split('#')[1];
-        }
         $('#hex-display-text').css('color', '#' + color);
         $('#hex-display-background').css('color', 'white');
         $('#hex-display-background').css('background-color', '#' + color);
