033409825b8814074edbd7678a0f799cbbb90463
Luke Leheny <lleheny@umass.edu>
2016-01-18 21:01:59 -0500

    yes

--- script.js
+++ script.js
@@ -17,11 +17,43 @@ var movies = [
   'Fargo'
 ];
 
+var shows = [
+  'Scrubs',
+  'Game of Thrones',
+  'Always Sunny',
+  'The X Files',
+  'Breaking Bad',
+  'That 70s Show',
+  'Arrested Development',
+  'Modern Family'
+];
+
+var games = [
+  'Minecraft',
+  'Beyond Two Souls',
+  'Far Cry 4',
+  'Heavy Rain',
+  'Kingdom Hearts',
+  'Ni No Kuni',
+  'Little Big Planet',
+  'The Last of Us',
+  'Animal Crossing',
+  'Fallout 3',
+  'Fallout New Vegas',
+  'Mass Effect'
+];
+
 $(document).ready(function() {
   $('input[type=button]').click(function() {
     $('#title').html('');
-  })
+  });
   $('#movie-button').click(function() {
     $('#title').html(movies[Math.floor(Math.random() * movies.length)]);
+  });
+  $('#show-button').click(function() {
+    $('#title').html(shows[Math.floor(Math.random() * shows.length)]);
+  });
+  $('#game-button').click(function() {
+    $('#title').html(games[Math.floor(Math.random() * games.length)]);
   });
 });
